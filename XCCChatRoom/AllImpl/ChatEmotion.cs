62f5e40ba44d6732ce1a4d4a65db6e7531788e73
xfe18827675401 <xfe18827675401@126.com>
2023-10-30 15:35:18 +0800

    更多表情包支持

--- XCCChatRoom/AllImpl/ChatEmotion.cs
+++ XCCChatRoom/AllImpl/ChatEmotion.cs
@@ -2,6 +2,6 @@
 {
     public static class ChatEmotion
     {
-        public static string[] EmotionNameList = { "amazed_face", "anguished_face", "anxious_face", "backhand_index_pointing_right", "backhand_index_pointing_up", "clapping_hands", "cloud", "clown_face", "confounded_face", "confused_face", "dog", "dog_face", "face_blowing_a_kiss", "face_savoring_food", "face_with_tongue", "face_without_mouth", "fire", "full_moon_face" };
+        public static string[] EmotionNameList = { "amazed_face", "anguished_face", "anxious_face", "backhand_index_pointing_right", "backhand_index_pointing_up", "bear_face", "bouquet", "chicken", "christmas_tree", "clapping_hands", "cloud", "clown_face", "collision", "confounded_face", "confused_face", "dog", "dog_face", "face_blowing_a_kiss", "face_savoring_food", "face_with_tongue", "face_without_mouth", "fire", "folded_hands", "full_moon_face", "pile_of_poo", "pouting_face", "rolling_on_the_floor_laughing", "sweat_droplets" };
     }
 }
